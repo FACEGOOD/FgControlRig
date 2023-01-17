589ae8dad0904cd345422bbcf2cca693da16aebf
Nero <wang.xiangyuan@facegood.cc>
2023-01-17 15:00:33 +0800

    init repo

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/FgControlRigLib/UHT/FgControlRigLib.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeFgControlRigLib_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_FgControlRigLib;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_FgControlRigLib()
+	{
+		if (!Z_Registration_Info_UPackage__Script_FgControlRigLib.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/FgControlRigLib",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xA3E4F8FE,
+				0x9A9C3E85,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_FgControlRigLib.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_FgControlRigLib.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_FgControlRigLib(Z_Construct_UPackage__Script_FgControlRigLib, TEXT("/Script/FgControlRigLib"), Z_Registration_Info_UPackage__Script_FgControlRigLib, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xA3E4F8FE, 0x9A9C3E85));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
